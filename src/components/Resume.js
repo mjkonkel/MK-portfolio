895c31f6f532cea909d317fb5bfe41edb9c08751
mjkonkel <mitchell.konkel@gmail.com>
2023-03-19 19:51:50 -0500

    updated resume page formatting

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,27 +7,35 @@ export default function Resume() {
         <div className="resume">
             <h1>Resume</h1>
             <a href={resumePDF} target="_blank" rel="noreferrer noopener"><button>Download my Resume</button></a>
-            
-            <h2>My Skills</h2>
-            <ul>
-                <p><b>Front-end:</b></p>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>jQuery</li>
-                <li>Bootstrap</li>
-                <li>responsive design</li>
 
-                <p><b>Back-end:</b></p>
-                <li>APIs</li>
-                <li>Node</li>
-                <li>Express</li>
-                <li>MySQL and Sequelize</li>
-                <li>MongoDB and Mongoose</li>
-                <li>REST</li>
-                <li>GraphQL</li>
-            </ul>
+            <h2>My Skills</h2>
+            <div className='list-cont'>
+                <div className='skills-list'>
+                    <ul>
+                        <p><b>Front-end:</b></p>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                        <li>jQuery</li>
+                        <li>Bootstrap</li>
+                        <li>responsive design</li>
+                    </ul>
+                </div>
+                <div className='skills-list'>
+                    <ul>
+                        <p><b>Back-end:</b></p>
+                        <li>APIs</li>
+                        <li>Node</li>
+                        <li>Express</li>
+                        <li>MySQL and Sequelize</li>
+                        <li>MongoDB and Mongoose</li>
+                        <li>REST</li>
+                        <li>GraphQL</li>
+                    </ul>
+                </div>
+            </div>
+
         </div>
     );
 
