7533f080f96e9fdf34743c951064e02a7037f913
mjkonkel <mitchell.konkel@gmail.com>
2023-06-20 18:33:00 -0500

    bio update

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,7 +8,8 @@ export default function AboutMe() {
         <div className="about">
             <h1>About Me</h1>
             <img src={profilePic} alt='profile pic' className='profilePic' />
-            <div id="about-text">Full stack web developer with a focus on the MERN stack. Currently based in Minneapolis, MN. Willing to relocate. Ready to create engaging, impactful web apps and to use code as a problem solving tool to make the web accessible and user-centered.</div>
+            <div class="about-text">After college, I was passionate about sustainable agriculture and envisioned myself on the forefront of a revolution in changing how we eat. I started by working at various urban farms, but found I wanted a more technical challenge. This eventually led me to coding, which allows me to think creatively and work with passionate colleagues to solve complex problems. Nonetheless, I believe my past experiences continue to inform the work I do every day with web development. My drive to make positive change and collaborate with a community of innovative thinkers and problem solvers remains. With coding, I look forward to being challenged every day knowing there is always something new to learn.</div>
+            <div class="about-text">Full stack web developer with a focus on the MERN stack. Currently based in Minneapolis, MN. Willing to relocate.</div>
         </div >
     );
 
